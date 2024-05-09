d2dbbcec475540b4b10a22df97258a2eba748e3c
Dharan Aditya <dharan.aditya@gmail.com>
2024-05-09 17:58:29 +0530

    add: ListNode and TreeNode structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
 fn main() {
     println!("{}", Solution::num_rescue_boats(vec![3,5,3,4], 5)); // 4
     println!("{}", Solution::num_rescue_boats(vec![1,3,3,4], 5)); // 3
